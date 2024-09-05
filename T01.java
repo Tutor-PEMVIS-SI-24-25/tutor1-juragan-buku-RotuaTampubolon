4812416737666e515f4ac9645cb8952701394431
RotuaTampubolon <rotuaimmanuela@gmail.com>
2024-09-04 21:28:41 -1200

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,10 @@
-// NIM - NAMA
+// 12S24028 - Rotua immanuela
 // NIM - NAMA
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    public static void main(String[] args) {
+    }
+}
